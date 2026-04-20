0ab32aa48c086c30080ec22ea81e46f0a8ea6fff
HannesMeiners <hannes_meiners@gmx.de>
2026-04-20 11:43:35 +0200

    überarbeitung der website zu php

---
+++ content.php
@@ -0,0 +1,10 @@
+<div class="wrapper">
+    <div class="release">
+        <div>
+            <p id="days"></p>
+            <p id="hours"></p>
+            <p id="minutes"></p>
+            <p id="seconds"></p>
+        </div>
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,11 @@
+<header class="row">
+    <div>Hallo</div>
+    <nav class="nav-with-button">
+        <ul class="header-nav">
+            <li><a class="nav-link" href="https://www.bbs-nienburg.de/" target="_blank">BBS-Nienburg</a></li>
+            <li>Das sind wir</li>
+            <li>Unser Tag</li>
+        </ul>
+    </nav>
+    <button class="darkmode">☀️</button>
+</header>
